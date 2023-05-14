0c42d006c8f2830e3cfc68cdede731d66611e4b9
BatistaTony <batistatomeoliveira96@gmail.com>
2023-05-14 19:22:25 +0100

    build a simple dice app with swift ui

--- DiceApp/ContentView.swift
+++ DiceApp/ContentView.swift
@@ -8,13 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var dices = DicesData()
+    
+    @State private var firstDiceIndex = 0
+    @State private var secondDiceIndex = 1
+    
+    func rollTheDice(){
+        
+        let valueOne = Int.random(in: 0..<6)
+        let valueTwo = Int.random(in: 0..<6)
+        
+        firstDiceIndex = valueOne
+        secondDiceIndex = valueTwo
+        
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("GreenBackground")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 100) {
+                
+                VStack {
+                    Image("DiceeLogo")
+                }
+                
+                Spacer()
+                
+                VStack {
+                    HStack(spacing: 50) {
+                        Image(dices.dices[firstDiceIndex])
+                        Image(dices.dices[secondDiceIndex])
+                    }
+                }
+                
+                Spacer()
+                
+                Button  {
+                    rollTheDice()
+                } label: {
+                    Text("Roll")
+                        .frame(width: 150, height: 50)
+                        .tint(.purple)
+                        .foregroundColor(.black)
+                        .background(Color.green)
+                        .cornerRadius(10)
+                        
+                }
+                .padding(.bottom, 50)
+            
+                
+            }
+            .padding(20)
+            
+          
         }
+        
         .padding()
     }
 }
---
+++ DiceApp/Model/DicesData.swift
@@ -0,0 +1,16 @@
+//
+//  DicesData.swift
+//  DiceApp
+//
+//  Created by Batista Tone on 14/05/23.
+//
+
+import Foundation
+
+
+
+struct DicesData {
+    
+    let dices = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive","DiceSix"]
+    
+}
