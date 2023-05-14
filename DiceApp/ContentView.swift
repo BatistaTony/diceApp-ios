1377537109fc48a07dec7222e12e3bafd06980b0
BatistaTony <batistatomeoliveira96@gmail.com>
2023-05-14 18:07:45 +0100

    Initial Commit

---
+++ DiceApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DiceApp
+//
+//  Created by Batista Tone on 14/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceApp/DiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppApp.swift
+//  DiceApp
+//
+//  Created by Batista Tone on 14/05/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
