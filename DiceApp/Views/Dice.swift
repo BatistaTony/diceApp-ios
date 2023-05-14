59d3ffecd6370e65ddf1ddcdbb23ec5a4b8efb9d
BatistaTony <batistatomeoliveira96@gmail.com>
2023-05-14 19:24:07 +0100

    chore: simple diceApp

---
+++ DiceApp/Views/Dice.swift
@@ -0,0 +1,28 @@
+//
+//  Dice.swift
+//  DiceApp
+//
+//  Created by Batista Tone on 14/05/23.
+//
+
+import SwiftUI
+
+
+struct Dice: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct Dice_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
