2e96e9d06f0d5c938783986c8ee9859a2e53315d
BatistaTony <batistatomeoliveira96@gmail.com>
2023-05-15 11:41:53 +0100

    make the layout adaptive

--- DiceApp/ContentView.swift
+++ DiceApp/ContentView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     
+    
+    @Environment(\.verticalSizeClass) var vSizeClass: UserInterfaceSizeClass?
+    @Environment(\.horizontalSizeClass) var hSizeClass: UserInterfaceSizeClass?
+    
     var dices = DicesData()
     
     @State private var firstDiceIndex = 0
@@ -24,57 +28,113 @@ struct ContentView: View {
         
     }
     
+    
     var body: some View {
-        ZStack {
-            Image("GreenBackground")
-                .resizable()
-                .scaledToFill()
-                .edgesIgnoringSafeArea(.all)
-                .ignoresSafeArea()
-            
-            VStack(spacing: 100) {
+        
+            ZStack {
                 
-                VStack {
-                    Image("DiceeLogo")
+                GeometryReader { geometry in
+                    
+                    Image("GreenBackground")
+                        .resizable()
+                        .scaledToFill()
+                        .edgesIgnoringSafeArea(.all)
+                        .ignoresSafeArea()
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                    
                 }
                 
-                Spacer()
-                
-                VStack {
-                    HStack(spacing: 50) {
-                        Image(dices.dices[firstDiceIndex])
-                        Image(dices.dices[secondDiceIndex])
-                    }
-                }
-                
-                Spacer()
-                
-                Button  {
-                    rollTheDice()
-                } label: {
-                    Text("Roll")
-                        .frame(width: 150, height: 50)
-                        .tint(.purple)
-                        .foregroundColor(.black)
-                        .background(Color.green)
-                        .cornerRadius(10)
+                GeometryReader { geometry in
+                    
+                    let hSize = geometry.size.height
+                    let wSize = geometry.size.width
+                    
+                    VStack {
                         
+                        VStack(spacing: 1) {
+                            
+                            
+                            if(vSizeClass == .regular){
+                                Spacer(minLength: hSize * 30/100)
+                                
+                                VStack {
+                                    Image("DiceeLogo")
+                                }
+                                
+                                Spacer(minLength: hSize * 20/100)
+                            }
+                           
+                            
+                            
+                            VStack {
+                                
+                               
+                                if(vSizeClass == .compact){
+                                    
+                                    Spacer(minLength: hSize * 35/100)
+                                 
+                                    VStack {
+                                        Image("DiceeLogo")
+                                    }
+                                    Spacer(minLength: hSize * 5/100)
+                                }
+                                
+                                HStack(spacing: 20 ) {
+                                    Image(dices.dices[firstDiceIndex])
+                                    Image(dices.dices[secondDiceIndex])
+                                }
+                                
+                            }
+                            
+                            
+                        }
+                        
+                        if(vSizeClass == .regular){
+                            Spacer(minLength: geometry.size.height * 30/100)
+                        }else{
+                            Spacer(minLength: geometry.size.height * 5/100)
+                        }
+                        
+                        
+                        
+                        Button  {
+                            rollTheDice()
+                        } label: {
+                            Text("Roll")
+                                .frame(width: 150, height: 50)
+                                .tint(.purple)
+                                .foregroundColor(.black)
+                                .background(Color.green)
+                                .cornerRadius(10)
+                            
+                        }
+                        
+                        Spacer(minLength: geometry.size.height * 3/100)
+                        
+                    }
+                    .frame(width: geometry.size.width, height: geometry.size.height * 70/100)
+                     
                 }
-                .padding(.bottom, 50)
-            
                 
             }
-            .padding(20)
-            
-          
-        }
+            .scaledToFill()
+            .padding()
         
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone SE (3rd generation)")
+        
+        ContentView()
+            .previewDevice("iPhone 14 Pro Max")
+
+        ContentView()
+            .previewDevice("iPhone 14 Pro Max")
+            .previewInterfaceOrientation(.landscapeRight)
+
+
     }
 }
